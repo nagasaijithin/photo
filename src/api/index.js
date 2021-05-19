39cd3da197954f144c320d04ddc2a770dda53bc2
nagasaijithin <nagasaijithin1@gmail.com>
2021-05-19 12:24:16 +0530

    fix the bug

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://api.unsplash.com/",
+  baseURL: "https://api.unsplash.com/",
   withCredentials: false,
 
   headers: {
     "Access-Control-Allow-Origin": "*",
-    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
     Authorization: "Client-ID GhgFPRjHq24CGnkTq8Yxf5xgWV_HHQ8e5ZDmsAmXeUg",
   },
 });
